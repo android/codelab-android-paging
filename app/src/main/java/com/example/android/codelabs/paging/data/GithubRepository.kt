fb007f3c752bb8b7ecae09294941e3c1a2c3585b
Florina Muntenescu <florinam@google.com>
2020-01-29 18:49:23 +0000

    Showing an infinite scrolling list, from network, with Flow

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -17,27 +17,28 @@
 package com.example.android.codelabs.paging.data
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import com.example.android.codelabs.paging.api.GithubService
 import com.example.android.codelabs.paging.api.searchRepos
 import com.example.android.codelabs.paging.model.Repo
 import com.example.android.codelabs.paging.model.RepoSearchResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 
 /**
  * Repository class that works with local and remote data sources.
  */
+@FlowPreview
+@ExperimentalCoroutinesApi
 class GithubRepository(private val service: GithubService) {
 
     // keep the list of responses
-    private val inMemoryCache = MutableLiveData<List<Repo>>()
+    private val inMemoryCache = ConflatedBroadcastChannel<RepoSearchResult>()
 
     // keep the last requested page. When the request is successful, increment the page number.
     private var lastRequestedPage = 1
-
-    // LiveData of network errors.
-    private val networkErrors = MutableLiveData<String>()
 
     // avoid triggering multiple requests in the same time
     private var isRequestInProgress = false
@@ -45,51 +46,46 @@ class GithubRepository(private val service: GithubService) {
     /**
      * Search repositories whose names match the query.
      */
-    fun search(query: String): RepoSearchResult {
+    suspend fun search(query: String): Flow<RepoSearchResult> {
         Log.d("GithubRepository", "New query: $query")
         lastRequestedPage = 1
         requestAndSaveData(query)
 
         // Get data from the in memory cache
         val data = reposByName(query)
-
-        return RepoSearchResult(data, networkErrors)
+        inMemoryCache.offer(RepoSearchResult(data))
+        return inMemoryCache.asFlow()
     }
 
-    fun requestMore(query: String) {
+    suspend fun requestMore(query: String) {
         requestAndSaveData(query)
     }
 
-    private fun requestAndSaveData(query: String) {
+    private suspend fun requestAndSaveData(query: String) {
         if (isRequestInProgress) return
 
         isRequestInProgress = true
-        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
-            // add the new result list to the existing list
-            val allResults = mutableListOf<Repo>()
-            inMemoryCache.value?.let { allResults.addAll(it) }
-            allResults.addAll(repos)
+        val apiResponse = searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE)
+        Log.d("GithubRepository", "response $apiResponse")
+        // add the new result list to the existing list
+        val allResults = mutableListOf<Repo>()
+        inMemoryCache.valueOrNull?.let { allResults.addAll(it.data) }
+        allResults.addAll(apiResponse.data)
 
-            inMemoryCache.postValue(allResults)
-            lastRequestedPage++
-            isRequestInProgress = false
-        }, { error ->
-            networkErrors.postValue(error)
-            isRequestInProgress = false
-        })
+        inMemoryCache.offer(RepoSearchResult(allResults, apiResponse.networkErrors))
+
+        lastRequestedPage++
+        isRequestInProgress = false
     }
 
-    private fun reposByName(query: String): LiveData<List<Repo>> {
-        return Transformations.switchMap(inMemoryCache) { repos ->
-            // from the in memory cache select only the repos whose name or description matches
-            // the query. Then order the results.
-            val filteredList = repos.filter {
-                it.name.contains(query, true) ||
-                        (it.description != null && it.description.contains(query, true))
-            }.sortedWith(compareByDescending<Repo> { it.stars }.thenBy { it.name })
-
-            MutableLiveData(filteredList)
-        }
+    private fun reposByName(query: String): List<Repo> {
+        // from the in memory cache select only the repos whose name or description matches
+        // the query. Then order the results.
+        val result = inMemoryCache.valueOrNull ?: return emptyList()
+        return result.data.filter {
+            it.name.contains(query, true) ||
+                    (it.description != null && it.description.contains(query, true))
+        }.sortedWith(compareByDescending<Repo> { it.stars }.thenBy { it.name })
     }
 
     companion object {
--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -16,18 +16,17 @@
 
 package com.example.android.codelabs.paging.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.example.android.codelabs.paging.data.GithubRepository
-import com.example.android.codelabs.paging.model.Repo
 import com.example.android.codelabs.paging.model.RepoSearchResult
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for the [SearchRepositoriesActivity] screen.
  * The ViewModel works with the [GithubRepository] to get the data.
  */
+@ExperimentalCoroutinesApi
+@FlowPreview
 class SearchRepositoriesViewModel(private val repository: GithubRepository) : ViewModel() {
 
     companion object {
@@ -35,13 +34,11 @@ class SearchRepositoriesViewModel(private val repository: GithubRepository) : Vi
     }
 
     private val queryLiveData = MutableLiveData<String>()
-    private val repoResult: LiveData<RepoSearchResult> = Transformations.map(queryLiveData) {
-        repository.search(it)
-    }
-
-    val repos: LiveData<List<Repo>> = Transformations.switchMap(repoResult) { it -> it.data }
-    val networkErrors: LiveData<String> = Transformations.switchMap(repoResult) { it ->
-        it.networkErrors
+    val repoResult: LiveData<RepoSearchResult> = Transformations.switchMap(queryLiveData) {
+        liveData<RepoSearchResult> {
+            val repos = repository.search(it).asLiveData(Dispatchers.Main)
+            emitSource(repos)
+        }
     }
 
     /**
@@ -55,7 +52,9 @@ class SearchRepositoriesViewModel(private val repository: GithubRepository) : Vi
         if (visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount) {
             val immutableQuery = lastQueryValue()
             if (immutableQuery != null) {
-                repository.requestMore(immutableQuery)
+                viewModelScope.launch {
+                    repository.requestMore(immutableQuery)
+                }
             }
         }
     }
