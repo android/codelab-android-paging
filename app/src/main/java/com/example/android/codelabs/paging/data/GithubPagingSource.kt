e395f8e5e071502ddce9fe8d4cc7a9a824699084
Florina Muntenescu <florinam@google.com>
2020-02-27 15:13:49 +0000

    Remove code no longer needed due to paging

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
@@ -17,11 +17,12 @@ class GithubPagingSource(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
         val position = params.key ?: 0
         val apiResponse = searchRepos(service, query, position, GithubRepository.NETWORK_PAGE_SIZE)
-        return when(apiResponse){
+        return when (apiResponse) {
             is RepoSearchResult.Success -> LoadResult.Page(
                     data = apiResponse.data,
-                    prevKey = position,
-                    nextKey = position + 1
+                    prevKey = if (position == 0) null else -1,
+                    // if we don't get any results, we consider that we're at the last page
+                    nextKey = if (apiResponse.data.isEmpty()) null else position + 1
             )
             is RepoSearchResult.Error -> LoadResult.Error(apiResponse.error)
         }
--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -21,12 +21,9 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.PagingDataFlow
 import com.example.android.codelabs.paging.api.GithubService
-import com.example.android.codelabs.paging.api.searchRepos
 import com.example.android.codelabs.paging.model.Repo
-import com.example.android.codelabs.paging.model.RepoSearchResult
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.Flow
 
 /**
@@ -36,68 +33,17 @@ import kotlinx.coroutines.flow.Flow
 @ExperimentalCoroutinesApi
 class GithubRepository(private val service: GithubService) {
 
-    // keep the list of all results received
-    private val inMemoryCache = mutableListOf<Repo>()
-
-    // keep channel of results. The channel allows us to broadcast updates so
-    // the subscriber will have the latest data
-    private val searchResults = ConflatedBroadcastChannel<RepoSearchResult>()
-
-    // keep the last requested page. When the request is successful, increment the page number.
-    private var lastRequestedPage = 1
-
-    // avoid triggering multiple requests in the same time
-    private var isRequestInProgress = false
-
     /**
      * Search repositories whose names match the query, exposed as a stream of data that will emit
      * every time we get more data from the network.
      */
-    suspend fun getSearchResultStream(query: String): Flow<PagingData<Repo>> {
+    fun getSearchResultStream(query: String): Flow<PagingData<Repo>> {
         Log.d("GithubRepository", "New query: $query")
-        lastRequestedPage = 1
-        requestAndSaveData(query)
 
-        val pagedList = PagingDataFlow(
+        return PagingDataFlow(
                 config = PagingConfig(pageSize = NETWORK_PAGE_SIZE),
                 pagingSourceFactory = { GithubPagingSource(service, query) }
         )
-
-        return pagedList
-    }
-
-    suspend fun requestMore(query: String) {
-        requestAndSaveData(query)
-    }
-
-    private suspend fun requestAndSaveData(query: String) {
-        if (isRequestInProgress) return
-
-        isRequestInProgress = true
-        val apiResponse = searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE)
-        Log.d("GithubRepository", "response $apiResponse")
-        // add the new result list to the existing list
-        when (apiResponse) {
-            is RepoSearchResult.Success -> {
-                inMemoryCache.addAll(apiResponse.data)
-                val reposByName = reposByName(query)
-                searchResults.offer(RepoSearchResult.Success(reposByName))
-            }
-            is RepoSearchResult.Error -> {
-                searchResults.offer(RepoSearchResult.Error(apiResponse.error))
-            }
-        }
-        lastRequestedPage++
-        isRequestInProgress = false
-    }
-
-    private fun reposByName(query: String): List<Repo> {
-        // from the in memory cache select only the repos whose name or description matches
-        // the query. Then order the results.
-        return inMemoryCache.filter {
-            it.name.contains(query, true) ||
-                    (it.description != null && it.description.contains(query, true))
-        }.sortedWith(compareByDescending<Repo> { it.stars }.thenBy { it.name })
     }
 
     companion object {
