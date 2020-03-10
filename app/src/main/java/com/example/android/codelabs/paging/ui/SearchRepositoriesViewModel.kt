divijgupta970/android-paging

Migrating to Paging 3.0

app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -17,6 +17,9 @@
 package com.example.android.codelabs.paging.ui
 
 import androidx.lifecycle.*
+import androidx.paging.PagedList
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.android.codelabs.paging.data.GithubRepository
 import com.example.android.codelabs.paging.model.RepoSearchResult
 import kotlinx.coroutines.*
@@ -34,9 +37,11 @@ class SearchRepositoriesViewModel(private val repository: GithubRepository) : Vi
     }
 
     private val queryLiveData = MutableLiveData<String>()
-    val repoResult: LiveData<RepoSearchResult> =  queryLiveData.switchMap {
+    val repoResult: LiveData<PagingData<RepoSearchResult>> =  queryLiveData.switchMap {
         liveData {
-            val repos = repository.getSearchResultStream(it).asLiveData(Dispatchers.Main)
+            val repos = repository.getSearchResultStream(it)
+                    .cachedIn(viewModelScope)
+                    .asLiveData(Dispatchers.Main)
             emitSource(repos)
         }
     }

android/codelab-android-paging

app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -16,9 +16,17 @@
 
 package com.example.android.codelabs.paging.ui
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.android.codelabs.paging.data.GithubRepository
-import com.example.android.codelabs.paging.model.RepoSearchResult
+import com.example.android.codelabs.paging.model.Repo
 import kotlinx.coroutines.*
 
 /**
@@ -34,9 +42,11 @@ class SearchRepositoriesViewModel(private val repository: GithubRepository) : Vi
     }
 
     private val queryLiveData = MutableLiveData<String>()
-    val repoResult: LiveData<RepoSearchResult> =  queryLiveData.switchMap {
+    val repoResult: LiveData<PagingData<Repo>> = queryLiveData.switchMap {
         liveData {
-            val repos = repository.getSearchResultStream(it).asLiveData(Dispatchers.Main)
+            val repos = repository.getSearchResultStream(it)
+                    .cachedIn(viewModelScope)
+                    .asLiveData(Dispatchers.Main)
             emitSource(repos)
         }
     }