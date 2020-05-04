ae70b4b4f267189da6dc6e8a689ea9e5e01a9bf1
Florina Muntenescu <florinam@google.com>
2020-05-04 11:25:29 +0100

    Adding a loading state footer

--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.codelabs.paging.ui
 
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
