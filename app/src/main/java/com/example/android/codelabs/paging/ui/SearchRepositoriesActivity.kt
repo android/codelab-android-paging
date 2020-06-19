6569a106fd63ff0eb045361c3f840dbe31a2e3f3
Dustin Lam <dustinlam@google.com>
2020-06-18 18:42:05 -0700

    Cleanup loadStateListener logic

--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
@@ -18,10 +18,10 @@ package com.example.android.codelabs.paging.ui
 
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.ExperimentalPagingApi
@@ -60,7 +60,7 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-// get the view model
+        // get the view model
         viewModel = ViewModelProvider(this, Injection.provideViewModelFactory(this))
                 .get(SearchRepositoriesViewModel::class.java)
 
@@ -85,41 +85,28 @@ class SearchRepositoriesActivity : AppCompatActivity() {
                 header = ReposLoadStateAdapter { adapter.retry() },
                 footer = ReposLoadStateAdapter { adapter.retry() }
         )
+
         adapter.addLoadStateListener { loadState ->
-            if (loadState.refresh !is LoadState.NotLoading) {
-                // We're refreshing: either loading or we had an error
-                // So we can hide the list
-                binding.list.visibility = View.GONE
-                binding.progressBar.visibility = toVisibility(loadState.refresh is LoadState.Loading)
-                binding.retryButton.visibility = toVisibility(loadState.refresh is LoadState.Error)
-            } else {
-                // We're not refreshing - we're either prepending or appending
-                // So we should show the list
-                binding.list.visibility = View.VISIBLE
-                binding.progressBar.visibility = View.GONE
-                binding.retryButton.visibility = View.GONE
-                // If we have an error, show a toast
-                val errorState = when {
-                    loadState.append is LoadState.Error -> {
-                        loadState.append as LoadState.Error
-                    }
-                    loadState.prepend is LoadState.Error -> {
-                        loadState.prepend as LoadState.Error
-                    }
-                    else -> {
-                        null
-                    }
-                }
-                errorState?.let {
-                    Toast.makeText(
-                            this,
-                            "\uD83D\uDE28 Wooops ${it.error}",
-                            Toast.LENGTH_LONG
-                    ).show()
-                }
+            // Only show the list if refresh succeeds.
+            binding.list.isVisible = loadState.refresh is LoadState.NotLoading
+            // Show loading spinner during initial load or refresh.
+            binding.progressBar.isVisible = loadState.refresh is LoadState.Loading
+            // Show the retry state if initial load or refresh fails.
+            binding.retryButton.isVisible = loadState.refresh is LoadState.Error
+
+            // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+            val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+            errorState?.let {
+                Toast.makeText(
+                        this,
+                        "\uD83D\uDE28 Wooops ${it.error}",
+                        Toast.LENGTH_LONG
+                ).show()
             }
         }
-
     }
 
     private fun initSearch(query: String) {
--- app/src/main/java/com/example/android/codelabs/paging/ui/UiUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.codelabs.paging.ui
-
-import android.view.View
-
-fun toVisibility(constraint: Boolean): Int = if (constraint) {
-    View.VISIBLE
-} else {
-    View.GONE
-}
