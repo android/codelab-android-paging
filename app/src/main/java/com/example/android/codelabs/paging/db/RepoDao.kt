sshlapakovvlad/android-paging

Step 13 - 19 Paging from network and database

app/src/main/java/com/example/android/codelabs/paging/db/RepoDao.kt
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.db
+
+import androidx.paging.DataSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.android.codelabs.paging.model.Repo
+
+@Dao
+interface RepoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(repos: List<Repo>)
+
+    @Query("SELECT * FROM repos WHERE " +
+            "name LIKE :queryString OR description LIKE :queryString " +
+            "ORDER BY stars DESC, name ASC")
+    fun reposByName(queryString: String): DataSource.Factory<Int, Repo>
+
+    @Query("DELETE FROM repos")
+    suspend fun clearRepos()
+
+}

android/codelab-android-paging

app/src/main/java/com/example/android/codelabs/paging/db/RepoDao.kt
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.codelabs.paging.db
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.android.codelabs.paging.model.Repo
+
+@Dao
+interface RepoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(repos: List<Repo>)
+
+    @Query("SELECT * FROM repos WHERE " +
+            "name LIKE :queryString OR description LIKE :queryString " +
+            "ORDER BY stars DESC, name ASC")
+    fun reposByName(queryString: String): PagingSource<Int, Repo>
+
+    @Query("DELETE FROM repos")
+    suspend fun clearRepos()
+
+}