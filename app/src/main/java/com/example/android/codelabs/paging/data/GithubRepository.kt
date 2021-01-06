ff6e9d2d0fa870959e853c580b66cf117b428458
Florina Muntenescu <florinam@google.com>
2021-01-06 11:21:48 +0000

    Setting initial load size. Fixes #79 and #135

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -42,6 +42,6 @@ class GithubRepository(private val service: GithubService) {
     }
 
     companion object {
-        private const val NETWORK_PAGE_SIZE = 50
+        const val NETWORK_PAGE_SIZE = 50
     }
 }
