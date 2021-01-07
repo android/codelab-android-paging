be549ac8ab03e80fd27d7312bae5a1bc12ee56b1
Florina Muntenescu <2998890+florina-muntenescu@users.noreply.github.com>
2021-01-07 11:10:25 +0000

    Setting initial load size. Fixes #79 and #135 (#143)

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
@@ -19,6 +19,7 @@ package com.example.android.codelabs.paging.data
 import androidx.paging.PagingSource
 import com.example.android.codelabs.paging.api.GithubService
 import com.example.android.codelabs.paging.api.IN_QUALIFIER
+import com.example.android.codelabs.paging.data.GithubRepository.Companion.NETWORK_PAGE_SIZE
 import com.example.android.codelabs.paging.model.Repo
 import retrofit2.HttpException
 import java.io.IOException
@@ -27,8 +28,8 @@ import java.io.IOException
 private const val GITHUB_STARTING_PAGE_INDEX = 1
 
 class GithubPagingSource(
-        private val service: GithubService,
-        private val query: String
+    private val service: GithubService,
+    private val query: String
 ) : PagingSource<Int, Repo>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
         val position = params.key ?: GITHUB_STARTING_PAGE_INDEX
@@ -36,10 +37,17 @@ class GithubPagingSource(
         return try {
             val response = service.searchRepos(apiQuery, position, params.loadSize)
             val repos = response.items
+            val nextKey = if (repos.isEmpty()) {
+                null
+            } else {
+                // initial load size = 3 * NETWORK_PAGE_SIZE
+                // ensure we're not requesting duplicating items, at the 2nd request
+                position + (params.loadSize / NETWORK_PAGE_SIZE)
+            }
             LoadResult.Page(
-                    data = repos,
-                    prevKey = if (position == GITHUB_STARTING_PAGE_INDEX) null else position - 1,
-                    nextKey = if (repos.isEmpty()) null else position + 1
+                data = repos,
+                prevKey = if (position == GITHUB_STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = nextKey
             )
         } catch (exception: IOException) {
             LoadResult.Error(exception)
