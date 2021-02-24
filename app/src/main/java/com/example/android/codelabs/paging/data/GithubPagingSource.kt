ad387af2d8eca49f467d58b463bcac8c4ad9892a
Guillem Roca <guillemroc@gmail.com>
2021-02-24 12:17:45 +0100

    Implement GithubPagingSource.kt

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
@@ -2,18 +2,43 @@ package com.example.android.codelabs.paging.data
 
 import androidx.paging.PagingSource
 import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.api.IN_QUALIFIER
 import com.example.android.codelabs.paging.model.Repo
+import retrofit2.HttpException
+import java.io.IOException
 
 class GithubPagingSource(
         private val service: GithubService,
         private val query: String
 ) : PagingSource<Int, Repo>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
-        TODO("Not yet implemented")
+        val position = params.key ?: GITHUB_STARTING_PAGE_INDEX
+        val apiQuery = query + IN_QUALIFIER
+        return try {
+            val response = service.searchRepos(apiQuery, position, params.loadSize)
+            val repos = response.items
+            val nextKey = if ((repos.isEmpty())) {
+                null
+            } else {
+                // initial load size = 3 * NETWORK_PAGE_SIZE
+                // ensure we're not requesting duplicating items, at the 2nd request
+                position + (params.loadSize / NETWORK_PAGE_SIZE)
+            }
+            LoadResult.Page(
+                    data = repos,
+                    prevKey = if (position == GITHUB_STARTING_PAGE_INDEX) null else position - 1,
+                    nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
     }
 
     companion object {
         // GitHub page API is 1 based: https://developer.github.com/v3/#pagination
         private const val GITHUB_STARTING_PAGE_INDEX = 1
+        private const val NETWORK_PAGE_SIZE = 50
     }
 }
